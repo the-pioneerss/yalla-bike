3d8a723454c291501a1f210f60e5d53b408c6bb4
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-09-26 17:54:55 +0300

    day3

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
 import { CardImg, Badge, Card, Button, Carousel, Container, Row, Col } from 'react-bootstrap';
-import Footer from './Footer';
 const NEWS = process.env.REACT_APP_BACKEND_URL_NEWS;
 export class Main extends Component {
   constructor(props) {
@@ -77,99 +76,59 @@ export class Main extends Component {
             <Row className="justify-content-center">
               <Col lg="12">
                 <Row className="row-grid">
-                  <Col lg="4">
-                    <Card className="card-lift--hover shadow border-0">
-                      <Card.Body className="py-4">
-                        <CardImg variant='top' src='https://bikerentalmanager.com/wp-content/uploads/BikeApp_DM_DepositphotosSmall-900x450.png' height='160' />
-                        <br></br><br></br>
-                        <h6 className="text-danger text-uppercase">
-                          Yalla Rent !!
-                        </h6>
-                        <p className="description mt-3">
-                          loremipsum loremipsum loremipsum loremipsum
-                          loremipsum loremipsum loremipsum loremipsum
-                        </p>
-                        <div>
-                          <Badge color="success" pill className="mr-1">
-                            Business
-                          </Badge>
-                          <Badge color="success" pill className="mr-1">
-                            Go
-                          </Badge>
-                          <Badge color="success" pill className="mr-1">
-                            useful
-                          </Badge>
-                        </div>
-                        <Button
-                          className="mt-4"
-                          variant="danger"
-                          href="https://horned-beast-animal.netlify.app/"
-                          target="_blank"
-                        >Go....
-                        </Button>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                  <Col lg="3">
+                  <Col lg="1">
                   </Col>
                   <Col lg="4">
                     <Card className="card-lift--hover shadow border-0">
-                      <Card.Body className="py-4">
-                        <CardImg variant='top' src='https://visitelizabethcity.com//images/event_photos/Tarwheel2019_riverfront.jpg' height='160' />
-                        <br></br><br></br>
-                        <h6 className="text-success text-uppercase">
-                          Yalaa Events !!
-                        </h6>
-                        <p className="description mt-3">
-                          loremipsum loremipsum loremipsum loremipsum
-                          loremipsum loremipsum loremipsum loremipsum
-                          loremipsum loremipsum loremipsum loremipsum
-                        </p>
-                        <div>
-                          <Badge color="success" pill className="mr-1">
-                            Family
-                          </Badge>
-                          <Badge color="success" pill className="mr-1">
-                            Enjoy
-                          </Badge>
-                          <Badge color="success" pill className="mr-1">
-                            Groups
-                          </Badge>
-                        </div>
-                        <Button
-                          className="mt-4"
-                          variant="success"
-                          href="https://our-passion.github.io/library/index"
-                          target="_blank"
-                        >
-                          Go....
-                        </Button>
-                      </Card.Body>
+                      <Button
+                        className="mt-4"
+                        variant="warning"
+                        href="https://our-passion.github.io/library/index"
+                        target="_blank"
+                      >
+                        <Card.Body className="py-4">
+                          <CardImg variant='top' src='https://bikerentalmanager.com/wp-content/uploads/BikeApp_DM_DepositphotosSmall-900x450.png' height='160' />
+                          <br></br><br></br>
+                          <h6 className="text-danger text-uppercase">
+                            Yalla Rent !!
+                          </h6>
+                          <p className="description mt-3">
+                            loremipsum loremipsum loremipsum loremipsum
+                            loremipsum loremipsum loremipsum loremipsum
+                          </p>
+                        </Card.Body>
+                      </Button>
                     </Card>
+                  </Col>
+                  <Col lg="2">
+                  </Col>
+                  <Col lg="4">
+                    <Card className="card-lift--hover shadow border-0">
+                      <Button
+                        className="mt-4"
+                        variant="success"
+                        href="https://our-passion.github.io/library/index"
+                        target="_blank"
+                      >
+                        <Card.Body className="py-4">
+                          <CardImg variant='top' src='https://visitelizabethcity.com//images/event_photos/Tarwheel2019_riverfront.jpg' height='160' />
+                          <br></br><br></br>
+                          <h6 className="text-success text-uppercase">
+                            Yalaa Events !!
+                          </h6>
+                          <p className="description mt-3">
+                            loremipsum loremipsum loremipsum loremipsum
+                            loremipsum loremipsum loremipsum loremipsum
+                          </p>
+                        </Card.Body>
+                      </Button></Card>
                   </Col>
                 </Row>
               </Col>
             </Row>
           </Container>
         </section>
-        <blockquote className="card-blockquote">
-          <svg
-            className="svg-bg success"
-            preserveAspectRatio="none"
-            viewBox="0 0 583 95"
-          >
-            <polygon
-              className="fill-default "
-            />
-            <polygon
-              variant='danger'
-              color='yellow'
-              className="bg-success"
-              opacity=".2"
-              points="0,42 583,95 683,0 0,95"
-            />
-          </svg>
-        </blockquote>
+        <br></br><br></br><br></br><br></br>
         <section className="section pb-0 bg-gradient-danger"></section>
         <section className="section bg-light">
           <Container>
@@ -180,24 +139,8 @@ export class Main extends Component {
                     alt="..."
                     src="https://i.pinimg.com/564x/80/6b/8b/806b8b775e4470621a684d59766c4699.jpg"
                     top
+                    style={{ height: 500 }}
                   />
-                  <blockquote className="card-blockquote bg-success">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="svg-bg-danger"
-                      preserveAspectRatio="none"
-                      viewBox="0 0 583 95"
-                    >
-                      <polygon
-                        className="fill-danger"
-                        opacity=".2"
-                        points="0,42 583,95 683,0 0,95"
-                      />
-                    </svg>
-                    <h6 className="display-6 font-weight-bold text-dark">
-                      Stay healthy and use bike
-                    </h6>
-                  </blockquote>
                 </Card>
               </Col>
               <Col md="5">
@@ -216,19 +159,17 @@ export class Main extends Component {
                     orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
                     orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
                     orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-                    orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-                    orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-                    orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-                    orem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
                   </p>
                 </div>
               </Col>
             </Row>
           </Container>
         </section>
-        <br></br><br></br><br></br><br></br>
+        <br></br><br></br><br></br><br></br><br></br><br></br>
         <section class='newSection'>
-          <br></br><br></br><br></br><br></br><br></br>
+          <br></br><br></br>
+          <h1 id='fontoff' className="text-center">Why We Are Doing This ?!</h1>
+          <br></br><br></br>
           <section class='section'>
             <div className='row'>
               {
@@ -238,7 +179,7 @@ export class Main extends Component {
                       <Card className="card-lift--hover shadow border-0">
                         <Card.Body className="py-4">
                           <br></br><br></br>
-                          <h6 variant='top' className="text-danger text-uppercase">{item.title}</h6>
+                          <h6 variant='top' className="text-danger text-uppercase">"{item.title}"</h6>
                           <p className="description mt-3">{item.description}</p>
                           <div>
                             <Badge color="primary" pill className="mr-1">
@@ -264,3 +205,34 @@ export class Main extends Component {
   }
 }
 export default withAuth0(Main);
+/*
+                        <div>
+                          <Badge color="success" pill className="mr-1">
+                            Family
+                          </Badge>
+                          <Badge color="success" pill className="mr-1">
+                            Enjoy
+                          </Badge>
+                          <Badge color="success" pill className="mr-1">
+                            Groups
+                          </Badge>
+                        </div>
+                         <blockquote className="card-blockquote">
+          <svg
+            className="svg-bg success"
+            preserveAspectRatio="none"
+            viewBox="0 0 583 95"
+          >
+            <polygon
+              className="fill-default "
+            />
+            <polygon
+              variant='danger'
+              color='yellow'
+              className="bg-success"
+              opacity=".2"
+              points="0,42 583,95 683,0 0,95"
+            />
+          </svg>
+        </blockquote>
+*/
