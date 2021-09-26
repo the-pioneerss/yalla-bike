09637b9f999f4c4f52f8d7c690a9cde9153c103e
Rand92 <s.rund@yahoo.com>
2021-09-26 04:36:47 +0300

    rand2

--- src/App.js
+++ src/App.js
@@ -8,35 +8,13 @@ import { Modal,Button} from 'react-bootstrap';
 import { createEvent } from '@testing-library/dom';
 
 export class App extends Component {
-constructor(props){
-  super(props);
-        this.state={
-            showModal :false
-        };
-      }
-    handleShow=()=>{
-      this.setState({showModal:true,})
-      
-    };
-    handleClose=()=>{
-      this.setState({
-        showModal:false,
-      })
-    }
+
  
   render() {
     return (
       <div>
         {/* <Events/> */}
-        <Event2 showModal={this.state.showModal} 
-     handleShow={this.handleShow}
-     handleClose={this.handleClose}/>
-     {this.state.showModal && (
-      <createEvent 
-      showModal={this.state.showModal} 
-    
-      handleClose={this.handleClose}/>
-    )}
+        <Event2 />
       </div>
     )
   }
--- src/events/Event2.js
+++ src/events/Event2.js
@@ -1,28 +1,107 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Carousel,Button, Modal} from 'react-bootstrap';
+import { Carousel, Button, Modal ,Form,Card} from 'react-bootstrap';
 import CreateEvent from './CreateEvent';
 import './events.css';
 export class Event2 extends Component {
- 
-    render() {
-        return (
-            <div className="container1">
-               <div className="image w-100"
-    />
-       <div className="container2">
-    <p id="text" >Get on your bike and join us in our 
-    fun Cycling events! whoul you like to create your own ! 
-</p>
-     
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    };
+  }
+  handleModal = () => {
+    this.setState({ show: !this.state.show })
+
+  };
+
+
+  render() {
+    return (<div className="main">
+      <div className="container1" >
+
+        <div className="container2">
+        <Card className="bg-dark text-white">
+  <Card.Img className="image"  />
+  <Card.ImgOverlay className="cardOverlay">
+    <Card.Text className="textCard">
+    IT'S TIME TO GET THE BIKES OUT AND START PEDALLING! SO MUCH FUN IS WAITING FOR YOU ON EVENTS!
+    </Card.Text>
+  </Card.ImgOverlay>
+</Card>
+
+        </div>
+        <Button className="btn-cs scaleOut" onClick={() => { this.handleModal() }}>
+          Create An Event 
+        </Button>
+        <Modal
+          show={this.state.show}
+          onHide={this.handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton onClick={() => { this.handleModal() }}>
+            <Modal.Title>Create Event</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <Form>
+          <Form.Label>Your Name :</Form.Label>
+          <Form.Control type="text" placeholder="Input Your Full Name .." />
+  <br />
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Input your active Email" />
+  </Form.Group>
+  <Form.Label>Event's Name :</Form.Label>
+  <Form.Control type="text" placeholder="Input Event's Name" />
+  <br />
+  <Form.Label>Location</Form.Label>
+  <Form.Control type="text" placeholder="Input Event's Location" />
+  <br />
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Further Information About The Event</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+</Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => { this.handleModal() }}>
+              Close
+            </Button>
+            <Button id="Button" className="noselect">Create! 
+           </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      <div className="events">
+        <h3> Upcoming Events That May Interest You:</h3>
+      <Card className="card">
+  <Card.Header>Spring road trip</Card.Header>
+  <Card.Body>
+    <Card.Title>IF YOU'RE 40'S, THIS IS FOR YOU!</Card.Title>
+    <Card.Text>
+    A long-distance journey on the road. Typically, road trips are long distances traveled by automobile..
+    <br/> Location: Alsalt ,Jordan.
+    <br/> Time: Saturday September 25, 2021
+    </Card.Text>
+    <Button className="cardBut">Join!</Button>
+  </Card.Body>
+  <Card.Header>Morning road trip</Card.Header>
+  <Card.Body>
+    <Card.Title>everyday morning trip</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+      <br/> Location: Amman ,Jordan.
+    <br/> Time: Saturday Octoper 25, 2021
+    </Card.Text>
+    <Button className="cardBut ">Join!</Button>
+  </Card.Body>
+</Card>
+      </div>
     </div>
-    <Button variant="primary" onClick={this.props.handleShow}>
-        Launch static backdrop modal
-      </Button>
-            </div>
-        
-        )
-    }
+
+    )
+  }
 }
 
 export default Event2
