174eddf71335ae57d37c66d4e6898afab4ba7b70
Rand92 <s.rund@yahoo.com>
2021-09-27 20:07:14 +0300

    monday2

--- src/events/Event2.js
+++ src/events/Event2.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel, Button, Modal, Form, Card } from 'react-bootstrap';
+import { Button, Modal, Form, Card } from 'react-bootstrap';
 import Dialog from 'react-bootstrap-dialog'
 // but first install npm i react-bootstrap-dialog --save 
 import './events.css';
 export class Event2 extends Component {
   constructor() {
     super();
-    this.onClick = this.onClick1.bind(this)
-    this.onClick = this.onClick2.bind(this)
+ 
     this.state = {
       show: false,
-     
+      buttonEnabled: true,
+      buttonEnabled2: true,
     };
+    
   }
+  
   handleModal = () => {
     this.setState({ show: !this.state.show })
 
   };
-  onClick1 () {
-    this.dialog.showAlert('THANK YOU! will send you an Email once it is approved.')
-  }
-  onClick2 () {
-    this.dialog.showAlert('Joined!')
-  }
+
+  onClick1=()=>{
+   
+    this.setState({buttonEnabled: false});
+    this.dialog.showAlert("joined! See you soon!")
+    console.log("joined!");   
+};
+onClick2=()=>{
+   
+  this.setState({buttonEnabled: true});
+  this.dialog.showAlert('Oops! Cancelled!')
+  console.log("Cancelled!");   
+};
+onClick3=()=>{
+  this.setState({buttonEnabled2: false});
+  this.dialog.showAlert("joined! See you soon!")
+  console.log("joined!");   
+};
+onClick4=()=>{
+this.setState({buttonEnabled2: true});
+this.dialog.showAlert('Ops! Cancelled!')
+console.log("Cancelled!");   
+};
   render() {
     return (<div className="main">
       <div className="container1" >
@@ -39,9 +58,11 @@ export class Event2 extends Component {
           </Card>
 
         </div>
-        <Button className="btn-cs scaleOut" onClick={() => { this.handleModal() }}>
+        <div className="mainButton">
+        <Button className="btn-cs scaleOut uu" onClick={() => { this.handleModal() }}>
           Create An Event
         </Button>
+        </div>
         <Modal
           show={this.state.show}
           onHide={this.handleClose}
@@ -66,6 +87,9 @@ export class Event2 extends Component {
               <Form.Label>Location</Form.Label>
               <Form.Control type="text" placeholder="Input Event's Location" />
               <br />
+              <Form.Label>Time: </Form.Label>
+              <Form.Control type="time" placeholder="Input Event's Location" />
+              <br />
               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Further Information About The Event</Form.Label>
                 <Form.Control as="textarea" rows={3} />
@@ -76,7 +100,7 @@ export class Event2 extends Component {
             <Button variant="secondary" onClick={() => { this.handleModal() }}>
               Close
             </Button>
-            <Button id="Button" className="noselect" onClick={() => { this.onClick1() }}>Create! 
+            <Button id="Button" className="noselect" onClick={() => { this.onClick1() }}>Create!
             </Button>
           </Modal.Footer>
         </Modal>
@@ -86,23 +110,31 @@ export class Event2 extends Component {
         <Card className="card">
           <Card.Header>Spring Road Trip</Card.Header>
           <Card.Body>
-            <Card.Title>IF YOU'RE 40'S, THIS IS FOR YOU!</Card.Title>
+            <Card.Title>IF YOU'RE 40's, THIS IS FOR YOU!</Card.Title>
             <Card.Text>
               A long-distance journey on the road. Typically, road trips are long distances traveled by automobile..
-              <br /> Location: Alsalt ,Jordan.
-              <br /> Time: Saturday September 25, 2021
+              <br /> <strong>Location: </strong>  Alsalt ,Jordan.
+              <br /> <strong>Time: </strong> 12:00pm-2:00pm ,Saturday September 25, 2021
             </Card.Text>
-            <Button className="cardBut"  onClick={() => { this.onClick2() }}>Join!</Button>
+            {this.state.buttonEnabled2 &&
+            <Button className="cardBut " onClick={this.onClick3} >Join!</Button>}
+            {!this.state.buttonEnabled2&& 
+            <Button className="cardBut "variant="success" onClick={this.onClick4}>Joined!</Button>}
           </Card.Body>
+          </Card>
+          <Card className="card">
           <Card.Header>Morning Road Trip</Card.Header>
           <Card.Body>
-            <Card.Title>everyday morning trip</Card.Title>
+            <Card.Title>Everyday morning trip</Card.Title>
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
-              <br /> Location: Amman ,Jordan.
-              <br /> Time: Saturday Octoper 25, 2021
+              <br /> <strong>Location: </strong> Amman ,Jordan.
+              <br /> <strong>Time: </strong>  8:00am-10:00am ,Saturday Octoper 25, 2021
             </Card.Text>
-            <Button className="cardBut "  onClick={() => { this.onClick2() }}>Join!</Button>
+            {this.state.buttonEnabled &&
+            <Button className="cardBut " onClick={this.onClick1} >Join!</Button>}
+            {!this.state.buttonEnabled&& 
+            <Button className="cardBut "variant="success" onClick={this.onClick2}>Joined!</Button>}
           </Card.Body>
         </Card>
       </div>
